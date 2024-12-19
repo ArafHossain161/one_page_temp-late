9130abe78acc9684e7b01c886ec31c45d1217dc8
Araf <arafhossain161@gmail.com>
2024-12-19 22:37:33 +0600

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,8 @@
+@extends('Layout.app')
+@section('content')
+    @include('component.hero')
+    @include('component.howWeDo')
+    @include('component.pricing')
+    @include('component.team')
+    @include('component.newsletter')
+@endsection
